a94684cd86abd5f0e2ab32207032904e2b1f2671
khrisnaina06 <muhammadkhrisna12@gmail.com>
2023-04-17 14:00:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# PRAKTIKUM 6 (OOP Lanjutan)
+
+**_Nama: Muhammad Khrisna faisal zuhri_** <br/>
+**_Nim : 312110177_** <br/>
+**_Kelas : TI.21.A2_** <br/>
+
+<br/><br/>
+
+## **Directory Path**
+
+<img src="screenshot/directory.png">
+
+<br/><br/>
+
+## **Directory Tree**
+
+```bash
+Lab6Web
+│
+├───.htaccess
+├───config.php
+├───index.php
+├───README.md
+├───route.php
+│
+├───class
+│    ├───database.php
+│    └───form.php
+│
+├───module
+│    ├───home.php
+│    └───artikel
+│        ├───create.php
+│        ├───delete.php
+│        ├───error.php
+│        └───update.php
+│
+├───screenshot
+│    └───directory.JPG
+│
+└───template
+     ├───footer.php
+     ├───form_insert.php
+     ├───form_update.php
+     ├───header.php
+     └───navbar.php
+```
---
+++ index.php
@@ -0,0 +1,55 @@
+
+<?php
+require_once "route.php";
+require_once "./class/database.php";
+$moduleName =
+  strpos(@$_REQUEST["url"], "/") ? str_split(@$_REQUEST["url"], strpos(@$_REQUEST["url"], "/"))[0] : @$_REQUEST['url'];
+
+$url = [
+  "home" => "./module/home.php",
+  "create" => "./module/artikel/create.php",
+  "update" => "./module/artikel/update.php",
+  "delete" => "./module/artikel/delete.php",
+];
+
+$routes = new Route($url);
+
+include_once './template/header.php';
+include_once './template/navbar.php';
+?>
+
+
+<div class="p-5 max-w-[1000px] m-auto">
+  <?php $routes->load($moduleName); ?>
+</div>
+
+
+<?php include_once './template/footer.php'; ?>
+<style>
+		footer {
+		  position: fixed;
+		  bottom: 0;
+		  width: 100%;
+		  text-align: center;
+		  background-color: #f2f2f2;
+		  padding: 10px;
+		  transition: all 0.3s ease-in-out;
+		}
+
+		@media screen and (max-width: 767px) {
+		  footer {
+		    position: static;
+		    margin-top: 20px;
+		  }
+		}
+
+		@media screen and (min-width: 768px) {
+		  footer {
+		    position: fixed;
+		    bottom: 0;
+		  }
+		}
+	</style>
+<footer>
+    <p>&copy; 2023, Muhammad khrisna faisal zuhri, Universitas Pelita Bangsa</p>
+</footer>
